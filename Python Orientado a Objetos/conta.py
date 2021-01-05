bb5bf820ff678d425e4c4fb8d856d8efc1e1d723
datachemistry <biorrefinaria@gmail.com>
2021-01-05 00:43:41 -0300

    Finalizando Aulas Etapa 3

--- Python Orientado a Objetos/conta.py
+++ Python Orientado a Objetos/conta.py
@@ -1,4 +1,20 @@
 
 class Conta:
     
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        print("Contruindo objeto ...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+    
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        
+    def depositar(self, valor):
+        self.saldo = self.saldo + valor
+        
+    def sacar(self, valor):
+        self.saldo = self.saldo - valor
+    
+    
