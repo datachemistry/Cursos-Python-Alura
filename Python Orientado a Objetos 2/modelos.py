205e3916b436dac5cd09118045ef9f785718cd39
datachemistry <biorrefinaria@gmail.com>
2021-01-05 23:52:12 -0300

    Update 05-01-21

--- Python Orientado a Objetos 2/modelos.py
+++ Python Orientado a Objetos 2/modelos.py
@@ -1,50 +1,59 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
 
-class Filme:
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+        
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
+    
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
-class Serie:
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+        
+    
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)           
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+       
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
-
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+lor = Filme('o senhor dos aneis', 2001, 200)
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+lor.dar_likes()
+lor.dar_likes()
+lor.dar_likes()
+lor.dar_likes()
+lor.dar_likes()
+
+filmes_e_series = [vingadores, atlanta, lor]
+    
+for programa in filmes_e_series:
+    print(programa)
