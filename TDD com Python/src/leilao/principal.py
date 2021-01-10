b76b443a9f0fafbb1388143487ff9793335cf83d
datachemistry <biorrefinaria@gmail.com>
2021-01-10 01:30:50 -0300

    Update 10-01-21 -- Finalizado Curso

--- TDD com Python/src/leilao/principal.py
+++ TDD com Python/src/leilao/principal.py
@@ -1,4 +1,4 @@
-from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+from dominio import Usuario, Lance, Leilao
 
 gui = Usuario('Gui')
 yuri = Usuario('Yuri')
@@ -14,7 +14,4 @@ leilao.lances.append(lance_do_gui)
 for lance in leilao.lances:
     print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
 
-avaliador = Avaliador()
-avaliador.avalia(leilao)
 
-print(f'O menor lance foi de {avaliador.menor_lance} e o maior lance foi de {avaliador.maior_lance}')
